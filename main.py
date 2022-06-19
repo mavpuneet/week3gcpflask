c155edc345efed3166197559137b1ad20554e395
Puneet Sethi from GCP <sethi.puneet@gmail.com>
2022-06-19 05:54:00 +0000

    changing hello world to hello universe

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello Universe!'
 
 
 if __name__ == '__main__':
